7751d21d918c0d95f206d0aa90769f4a63f5ad97
Petru Mihailov <petru.mihailov@yahoo.fr>
2024-12-12 12:41:02 +0100

    Add event listener to arrow left and right

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,19 @@
 const slides = [
   {
-    image: "slide1.jpg",
+    image: "./assets/images/slideshow/slide1.jpg",
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
   },
   {
-    image: "slide2.jpg",
+    image: "./assets/images/slideshow/slide2.jpg",
     tagLine:
       "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
   },
   {
-    image: "slide3.jpg",
+    image: "./assets/images/slideshow/slide3.jpg",
     tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
   },
   {
-    image: "slide4.png",
+    image: "./assets/images/slideshow/slide4.png",
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
@@ -24,9 +24,38 @@ console.log(divArrowLeft);
 let divArrowRight = document.querySelector(".arrow_right");
 console.log(divArrowRight);
 
-let listeDots = document.querySelectorAll(".dot");
+let listeDots = document.querySelectorAll(".dots .dot");
 console.log(listeDots);
 
-for (let i = 0; i < listeDots.length; i++) {
-  console.log(listeDots[i]);
-}
+let bannerImmage = document.querySelector(".banner-img");
+
+let bannerTitle = document.querySelector("#banner p");
+console.log(bannerTitle, "bannerTitle");
+
+let currentIndex = 0;
+
+divArrowRight.addEventListener("click", () => {
+  listeDots[currentIndex].classList.remove("dot_selected");
+  currentIndex++;
+
+  if (currentIndex > slides.length - 1) {
+    currentIndex = 0;
+  }
+
+  listeDots[currentIndex].classList.add("dot_selected");
+  bannerImmage.src = slides[currentIndex].image;
+  bannerTitle.innerHTML = slides[currentIndex].tagLine;
+});
+
+divArrowLeft.addEventListener("click", () => {
+  listeDots[currentIndex].classList.remove("dot_selected");
+  currentIndex--;
+
+  if (currentIndex < 0) {
+    currentIndex = slides.length - 1;
+  }
+
+  listeDots[currentIndex].classList.add("dot_selected");
+  bannerImmage.src = slides[currentIndex].image;
+  bannerTitle.innerHTML = slides[currentIndex].tagLine;
+});
